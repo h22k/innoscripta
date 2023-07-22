c0d53f908130eb56c5e9136f8a464f7f11a81a72
Halil Hakan Karabay <hakankarabay@icloud.com>
2023-07-22 22:44:39 +0300

    feat: finished RegisterController.php

--- backend/app/Http/Controllers/Api/Auth/RegisterController.php
+++ backend/app/Http/Controllers/Api/Auth/RegisterController.php
@@ -4,14 +4,28 @@ namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\Auth\RegisterRequest;
+use App\Http\Response\ApiResponse;
+use App\Services\Api\Auth\RegisterService;
+use Exception;
+use Illuminate\Http\JsonResponse;
 
 class RegisterController extends Controller
 {
     /**
-     * Handle the incoming request.
+     * @param  RegisterRequest  $request
+     * @param  RegisterService  $registerService
+     * @return JsonResponse
+     * @throws Exception
      */
-    public function __invoke(RegisterRequest $request)
+    public function __invoke(RegisterRequest $request, RegisterService $registerService): JsonResponse
     {
-        //
+        $registeredUser = $registerService->register($request->validated());
+
+        return ApiResponse::created([
+            'user'       => $registeredUser,
+            'token'      => $registeredUser->createToken(),
+            'type'       => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+        ]);
     }
 }
