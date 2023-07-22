3670c785a220e94bff12971d4b5fb47f08aa168d
Halil Hakan Karabay <hakankarabay@icloud.com>
2023-07-22 22:44:21 +0300

    feat: finished RegisterService.php

--- backend/app/Services/Api/Auth/RegisterService.php
+++ backend/app/Services/Api/Auth/RegisterService.php
@@ -2,18 +2,34 @@
 
 namespace App\Services\Api\Auth;
 
-class RegisterService
+use App\Models\User;
+use Exception;
+
+final class RegisterService
 {
 
     /**
      *
      *
      * @param  array  $credentials
-     * @return string|null
+     * @return User
+     * @throws Exception
      */
-    public function register(array $credentials): ?string
+    public function register(array $credentials): User
     {
+        try {
+            // with this approach we are making sure that credentials array includes name, email, password keys
+            [
+                'name'     => $name,
+                'email'    => $email,
+                'password' => $password
+            ] = $credentials;
+        } catch (Exception $exception) {
+            throw new Exception('Something went wrong!');
+        }
 
+        // we have a global exception handler, so we don't need to worry about here.
+        return User::create(compact('name', 'email', 'password'));
     }
 
 }
