dfc728112a8f60cf45a19b67487a958960a972b3
Halil Hakan Karabay <hakankarabay@icloud.com>
2023-07-23 19:37:25 +0300

    feat: added login route

--- backend/routes/api/v1/auth.php
+++ backend/routes/api/v1/auth.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\LoginController;
 use App\Http\Controllers\Api\Auth\RegisterController;
 
 Route::name('auth.')->group(static function () {
 
-    Route::post('register', RegisterController::class)->middleware('guest')->name('register');
+    Route::middleware('guest')->group(static function () {
+        Route::post('register', RegisterController::class)->name('register');
+        Route::post('login', LoginController::class)->name('login');
+    });
 
 });
