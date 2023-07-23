388ff2ce7e137b0cbea514dfd2abba273e39ccf2
Halil Hakan Karabay <hakankarabay@icloud.com>
2023-07-23 19:49:30 +0300

    feat: added MESSAGE const to WrongCredentialException.php

--- backend/app/Exceptions/WrongCredentialException.php
+++ backend/app/Exceptions/WrongCredentialException.php
@@ -7,8 +7,9 @@ use Symfony\Component\HttpFoundation\Response;
 
 class WrongCredentialException extends Exception
 {
+    const MESSAGE = 'Credentials are wrong! Try again.';
     public function __construct()
     {
-        parent::__construct('Credentials are wrong! Try again.', Response::HTTP_BAD_REQUEST);
+        parent::__construct(self::MESSAGE, Response::HTTP_BAD_REQUEST);
     }
 }
