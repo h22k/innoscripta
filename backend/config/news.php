b011b0aee5d3e78c5295da93c1b8d0943531e95c
Halil Hakan Karabay <hakankarabay@icloud.com>
2023-07-25 18:06:29 +0300

    feat: upgraded news.php

--- backend/config/news.php
+++ backend/config/news.php
@@ -3,13 +3,24 @@
 use App\Components\News\Enums\NewsSources;
 
 return [
-
     'available_sources' => NewsSources::values(),
 
     'sources' => [
 
         NewsSources::NEW_YORK_TIMES->value => [
+            'method_name' => 'newYorkTimes',
+            'base_url'    => 'https://api.nytimes.com',
+            'url_params'  => [
+                'api-key' => env('NEW_YORK_TIMES_API_KEY'),
+            ],
+        ],
 
+        NewsSources::NEWS_API->value => [
+            'method_name' => 'newsApi',
+            'base_url'    => 'https://newsapi.org/v2',
+            'url_params'  => [
+                'apiKey' => env('NEWSAPI_API_KEY'),
+            ],
         ],
 
     ],
