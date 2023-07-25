00db042f03a8f3e0ff4b8c518c8a59d4b76cf317
Halil Hakan Karabay <hakankarabay@icloud.com>
2023-07-25 18:08:11 +0300

    feat: filled NewsClientFactory.php

---
+++ backend/app/Components/News/NewsClientFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Components\News;
+
+use App\Components\News\Enums\NewsSources;
+use App\Exceptions\InvalidNewsSourceException;
+use Illuminate\Http\Client\PendingRequest;
+
+class NewsClientFactory
+{
+
+    /**
+     * @param  string  $source
+     * @return PendingRequest
+     * @throws InvalidNewsSourceException
+     */
+    public static function getClient(string $source): PendingRequest
+    {
+        $availableSources = config('news.available_sources');
+
+        if ( ! in_array($source, $availableSources)) {
+            throw new InvalidNewsSourceException($source);
+        }
+
+        return match ($source) {
+            NewsSources::NEW_YORK_TIMES->value => \Http::newYorkTimes(),
+        };
+    }
+
+}
