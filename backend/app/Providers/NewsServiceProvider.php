40250fef1a016b6a48267f4996267b0119cba289
Halil Hakan Karabay <hakankarabay@icloud.com>
2023-07-25 18:05:35 +0300

    feat: added NewsServiceProvider.php

---
+++ backend/app/Providers/NewsServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class NewsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $sources = config('news.sources');
+
+        $this->addMacrosToHttp($sources);
+
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+
+    private function addMacrosToHttp(array $sources): void
+    {
+        foreach ($sources as $source) {
+            \Http::macro($source['method_name'], fn () => \Http::baseUrl($source['base_url'])->withQueryParameters($source['url_params']));
+        }
+    }
+}
