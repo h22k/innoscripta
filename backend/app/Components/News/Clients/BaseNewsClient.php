da2d1fbb305f59471e559df253d19ed2d611eb33
Halil Hakan Karabay <hakankarabay@icloud.com>
2023-07-26 01:07:52 +0300

    fix: added some methods and changed logic in BaseNewsClient.php

--- backend/app/Components/News/Clients/BaseNewsClient.php
+++ backend/app/Components/News/Clients/BaseNewsClient.php
@@ -2,25 +2,88 @@
 
 namespace App\Components\News\Clients;
 
+use App\Components\News\Helpers\PoolOption;
+use GuzzleHttp\Promise\PromiseInterface;
 use Illuminate\Http\Client\HttpClientException;
 use Illuminate\Http\Client\PendingRequest;
+use Illuminate\Http\Client\Pool;
 use Illuminate\Http\Client\Response;
 use Throwable;
 
 abstract class BaseNewsClient implements NewsHTTPClient
 {
-    public function __construct(protected PendingRequest $request) {}
+    public function __construct(protected PendingRequest $request)
+    {
+    }
 
     /**
-     * @return Response
+     * @return array
      */
-    abstract protected function getResponse(): Response;
+    abstract protected function getResponse(): array;
 
     /**
      * @param  array  $news
      * @return array
      */
     abstract protected function extractNews(array $news): array;
+
+    abstract protected function getNewsUri(): string;
+
+    /**
+     * @param  array  $options
+     * @return array
+     * @throws Throwable
+     */
+    protected function getNewsFromSource(array $options = []): array
+    {
+        $response = $this->prepareRequest($this->request, $options);
+
+        throw_if($response->failed(),
+            new HttpClientException(sprintf('Response failed! |||| ERROR_JSON::: %s',
+                json_encode([$response->body(), $response->status(), $response->effectiveUri()]))));
+
+        return $response->json();
+    }
+
+    /**
+     * @param  array  $options
+     * @param  PoolOption  $poolOption
+     * @return array<PromiseInterface | Response>
+     */
+    protected function getNewsFromSourceWithPool(array $options, PoolOption $poolOption): array
+    {
+        /**
+         * @var array<PromiseInterface | Response> $responses
+         */
+        $responses = $this->request->pool(function (Pool $pool) use ($options, $poolOption) {
+            for ($i = $poolOption->getStartIndex(); $i <= $poolOption->getFinishIndex(); $i++) {
+                \Arr::set($options, $poolOption->getIncreasedKey(), $i);
+                $requests[] = $this->prepareRequest($pool, $options);
+            }
+            return $requests ?? [];
+        });
+
+        return $responses;
+    }
+
+    /**
+     * @param  PendingRequest|Pool  $request
+     * @param  array  $options
+     * @return Response | PromiseInterface
+     */
+    protected function prepareRequest(PendingRequest | Pool $request, array $options): Response | PromiseInterface
+    {
+        if ($request instanceof Pool) {
+            $requestOptions = $this->request->getOptions();
+            $request = $request
+                ->baseUrl(\Arr::get($requestOptions, 'base_url'))
+                ->withQueryParameters(\Arr::get($requestOptions, 'url_params'));
+        }
+        return $request
+            ->withUrlParameters(\Arr::get($options, 'urlParams', []))
+            ->withQueryParameters(\Arr::get($options, 'queryParams', []))
+            ->get($this->getNewsUri());
+    }
 
     /**
      * @return array
@@ -30,8 +93,6 @@ abstract class BaseNewsClient implements NewsHTTPClient
     {
         $response = $this->getResponse();
 
-        throw_if($response->failed(), new HttpClientException(sprintf('Response failed! |||| ERROR_JSON::: %s', json_encode($response->body()))));
-
-        return $this->extractNews($response->json());
+        return $this->extractNews($response);
     }
 }
