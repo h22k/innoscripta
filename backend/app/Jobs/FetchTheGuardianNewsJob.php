87a87447fa287726afc8696053b18457f061c960
Halil Hakan Karabay <hakankarabay@icloud.com>
2023-07-26 06:54:23 +0300

    feat: added job field to news config & created jobs & implemented bindMethod from NewsServiceProvider.php

---
+++ backend/app/Jobs/FetchTheGuardianNewsJob.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Jobs\Helpers\NewsFetcher;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class FetchTheGuardianNewsJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels, NewsFetcher;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
--- backend/app/Providers/NewsServiceProvider.php
+++ backend/app/Providers/NewsServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Components\News\NewsClientFactory;
+use App\Components\News\NewsContext;
+use App\Components\News\NewsConverterFactory;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 
 class NewsServiceProvider extends ServiceProvider
@@ -14,7 +18,7 @@ class NewsServiceProvider extends ServiceProvider
         $sources = config('news.sources');
 
         $this->addMacrosToHttp($sources);
-
+        $this->bindJobMethods($sources);
     }
 
     /**
@@ -23,6 +27,18 @@ class NewsServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+    }
+
+    private function bindJobMethods(array $sources)
+    {
+        foreach ($sources as $sourceKey => $source) {
+            $this->app->bindMethod([\Arr::get($source, 'job'), 'handle'], function ($job, Application $application) use ($sourceKey) {
+                $client = NewsClientFactory::getClient($sourceKey);
+                $converter = NewsConverterFactory::getConverter($sourceKey);
+
+                return $job->handle(new NewsContext($client, $converter), $sourceKey);
+            });
+        }
     }
 
     private function addMacrosToHttp(array $sources): void
