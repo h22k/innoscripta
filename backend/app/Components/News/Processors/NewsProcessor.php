25f8eafa3e08924507f589fcc3efdca4ab863ae1
Halil Hakan Karabay <hakankarabay@icloud.com>
2023-07-26 19:33:39 +0300

    feat: added new methods to NewsProcessor.php

--- backend/app/Components/News/Processors/NewsProcessor.php
+++ backend/app/Components/News/Processors/NewsProcessor.php
@@ -3,7 +3,11 @@
 namespace App\Components\News\Processors;
 
 use App\Components\News\Converters\NewsConvertable;
+use App\Models\Author;
+use App\Models\Category;
 use App\Models\News;
+use App\Models\Source;
+use Illuminate\Database\Eloquent\Model;
 
 class NewsProcessor implements NewsProcessable
 {
@@ -16,6 +20,43 @@ class NewsProcessor implements NewsProcessable
     }
 
     /**
+     * @param  array  $fields
+     * @param  string  $modelNameSpace
+     * @return Model
+     */
+    private function getModel(array $fields, string $modelNameSpace): Model
+    {
+        return $modelNameSpace::where($fields)->firstOrCreate($fields);
+    }
+
+    /**
+     * @param  array  $fields
+     * @return Author | Model
+     */
+    private function getAuthor(array $fields): Author | Model
+    {
+        return $this->getModel($fields, Author::class);
+    }
+
+    /**
+     * @param  array  $fields
+     * @return Source|Model
+     */
+    private function getSource(array $fields): Source | Model
+    {
+        return $this->getModel($fields, Source::class);
+    }
+
+    /**
+     * @param  array  $fields
+     * @return Category|Model|null
+     */
+    private function getCategory(array $fields): Category | Model | null
+    {
+        return count($fields) === 0 ? null : $this->getModel($fields, Category::class);
+    }
+
+    /**
      * @return News[]
      */
     public function process(): array
@@ -24,7 +65,17 @@ class NewsProcessor implements NewsProcessable
 
         foreach ($this->convertedNews as $convertedNewsValue) {
             if ( ! News::whereTitle($convertedNewsValue['title'])->exists()) {
-                $news[] = News::create($convertedNewsValue);
+                $meta = \Arr::get($convertedNewsValue, 'meta');
+
+                $source = $this->getSource(\Arr::get($meta, 'source'));
+                $author = $this->getAuthor(array_merge(\Arr::get($meta, 'author'), ['source_id' => $source->id]));
+                $category = $this->getCategory(\Arr::get($meta, 'category') ?? []);
+
+                if ($category) {
+                    $convertedNewsValue['category_id'] = $category->id;
+                }
+
+                $news[] = $author->articles()->create($convertedNewsValue);
             }
         }
 
